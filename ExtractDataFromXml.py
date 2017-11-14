ae7d07e4cccf541a70def5f9b6961d8357a91bfa
syhamza <syhamza@microsoft.com>
2017-11-14 11:13:44 -0800

    minor changes

--- ExtractDataFromXml.py
+++ ExtractDataFromXml.py
@@ -3,26 +3,31 @@ import fileinput
 import xml.etree.ElementTree as ET
 import csv
 
+
 def ParseXML(file_location):
     tree = ET.parse(file_location)
     root = tree.getroot()
     listOfData = list()
-    for product in root.iter('Product'):
-        product_config_name = product.attrib['ProductConfigName']
-        pkpn = product.find('PKPN')
-        version = product.attrib['Version']
-        product_type = product.attrib["{http://www.w3.org/2001/XMLSchema-instance}type"]
-        # product_type = None
-        # print(product.attrib)
-        if(pkpn != None):
-            data = (product_config_name, version, product_type, pkpn.text)
-            listOfData.append(data)
+    for offer_package in root.iter('OfferPackage'):
+        offer_id = offer_package.attrib['OfferId']
+        product_groups = offer_package.find('ProductGroups')
+        for product_group in product_groups.iter('ProductGroup'):
+            category = product_group.attrib['Category']
+            for product in product_groups.iter('Product'):
+                product_config_name = product.attrib['ProductConfigName']
+                pkpn = product.find('PKPN')
+                version = product.attrib['Version']
+                product_type = product.attrib["{http://www.w3.org/2001/XMLSchema-instance}type"]
+                if(pkpn != None):
+                    data = (offer_id, category, product_config_name,
+                            version, product_type, pkpn.text)
+                    listOfData.append(data)
     with open('OfferData.csv', 'w') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow(['ProductConfigName', 'Version', 'Type', 'PKPN'])
+        writer.writerow(
+            ['OfferId', 'Category', 'ProductConfigName', 'Version', 'Type', 'PKPN'])
         for data in listOfData:
             writer.writerow(data)
-
 
 
 if __name__ == "__main__":
